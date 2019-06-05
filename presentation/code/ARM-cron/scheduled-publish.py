6078ca0842b34f98264f8c0f8906a0e4071806ef
dpliakos <dimitrispl96@gmail.com>
2019-06-05 23:03:41 +0300

    feat: Add the cron job.

---
+++ presentation/code/ARM-cron/scheduled-publish.py
@@ -0,0 +1,48 @@
+import time
+import schedule
+import paho.mqtt.client as paho
+
+BROKER = "192.168.1.2"
+PORT=1883
+TOPIC="house/common/livingroom/lights"
+
+LIGHTS_STATUS = False
+
+def turn_all_on():
+    payload = "on"
+    ret= client1.publish("{}".format(TOPIC),"{}".format(payload))  # publish
+
+def turn_all_off():
+    payload = "off"
+    ret= client1.publish("{}".format(TOPIC),"{}".format(payload))  # publish
+
+
+def fake_decide_from_time():
+    print("Fake decision make")
+
+    global LIGHTS_STATUS
+    if LIGHTS_STATUS:
+        turn_all_off()
+        LIGHTS_STATUS = False;
+    else:
+        turn_all_on()
+        LIGHTS_STATUS = True;
+
+def on_publish(client,userdata,result):             #create function for callback
+    print("data published \n")
+    pass
+
+
+
+
+
+client1= paho.Client("control1")                           #create client object
+client1.on_publish = on_publish                          #assign function to callback
+client1.connect(BROKER, PORT)                                 #establish connection
+
+# define the cron
+schedule.every(2).seconds.do(fake_decide_from_time)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
